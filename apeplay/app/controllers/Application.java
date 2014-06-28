8014c476ea951e0ce564d38d639a856a84d2f99a
pestusan <apester@gmx.net>
2014-06-28 22:08:53 +0200

    View added

--- apeplay/app/controllers/Application.java
+++ apeplay/app/controllers/Application.java
@@ -1,14 +1,14 @@
 package controllers;
 
+import models.ToDo;
 import play.*;
 import play.mvc.*;
-
 import views.html.*;
 
 public class Application extends Controller {
 
     public static Result index() {
-        return ok(index.render("Hello playwell!"));
+        return ok(index.render(ToDo.findTodosByUserEMail("bob@example.com")));
     }
 
 }
